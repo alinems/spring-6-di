1829d51d7c046dcc226b4e73a8f1f4a6d858c633
alinems <5702162+alinems@users.noreply.github.com>
2023-07-24 14:58:01 +0200

    lecture 35

--- src/main/java/guru/springframework/spring6di/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/spring6di/controllers/SetterInjectedController.java
@@ -1,16 +1,21 @@
 package guru.springframework.spring6di.controllers;
 
 import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetterInjectedController {
 
     private GreetingService greetingService;
 
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
+        System.out.println("SetterInjectedController.setGreetingService");
         this.greetingService = greetingService;
     }
 
-    public String sayHello() {
+    public String sayHello(){
         return greetingService.sayGreeting();
     }
 }
