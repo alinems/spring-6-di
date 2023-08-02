93a9c383094cd7df9d2c5fa6d81e5bf198b92774
alinems <5702162+alinems@users.noreply.github.com>
2023-08-02 21:12:29 +0200

    lecture 41 + changes to match original repo

--- src/test/java/guru/springframework/spring6di/controllers/EnvironmentControllerTest.java
+++ src/test/java/guru/springframework/spring6di/controllers/EnvironmentControllerTest.java
@@ -3,14 +3,20 @@ package guru.springframework.spring6di.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles({"dev", "EN"})
 @SpringBootTest
-public class EnvironmentControllerTest {
+class EnvironmentControllerTest {
+
     @Autowired
     EnvironmentController controller;
 
     @Test
     void getEnvironment() {
         System.out.println(controller.getEnvironment());
+
     }
 }
